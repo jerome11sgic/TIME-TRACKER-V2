b21ca16e1405c705ffd194b88a05f9211d2ca46b
sujaany <ssujaany09@gmail.com>
2019-06-05 13:58:54 +0530

    changes takes place in user email

--- src/message.php
+++ src/message.php
@@ -1,41 +1,57 @@
 <?php
-class Message{
-    private $errmessage=array();
-    private $successmessage=array();
-    private $errMsgcounter=0;
-    private $successMsgcounter=0;
+class Message
+{
+    private $errmessage = array();
+    private $successmessage = array();
+    private $errMsgcounter = 0;
+    private $successMsgcounter = 0;
     private $type;
 
-   
-    public function pushErrorMsg($msg){
-        $this->errmessage[]=$msg;
+
+    public function pushErrorMsg($msg)
+    {
+        $this->errmessage[] = $msg;
         $this->errMsgcounter++;
     }
 
-    public function pushSuccessMsg($msg){
-        $this->successmessage[]=$msg;
+    public function pushSuccessMsg($msg)
+    {
+        $this->successmessage[] = $msg;
         $this->successMsgcounter++;
     }
 
-    public function getErrMsgCount(){
+    public function getErrMsgCount()
+    {
         return $this->errMsgcounter;
     }
 
-    public function printMessage(){
-        if($this->getErrMsgCount()==0){
-            $this->type='success';
-        }else{
-            $this->type='err';
-        }
-       return array(
-                'type'=>$this->type,
-                'errorMsgcounts'=>$this->errMsgcounter,
-                'errorMessage'=>$this->errmessage,
-                'successMsgcounts'=>$this->successMsgcounter,
-                'successMessage'=>$this->successmessage);
+    public function getSuccessMsgCount()
+    {
+        return $this->successMsgcounter;
     }
 
-    public function printJsonMsg(){
+    public function printMessage()
+    {
+        if ($this->getErrMsgCount() == 0 && $this->getSuccessMsgCount() > 0) {
+            $this->type  = 'success';
+        } else if ($this->getErrMsgCount() > 0 && $this->getSuccessMsgCount() > 0) {
+            $this->type  = 'success-err';
+        } else if ($this->getErrMsgCount() == 0 && $this->getSuccessMsgCount() == 0) {
+            $this->type  = 'no-changes';
+        } else {
+            $this->type  = 'err';
+        }
+        return array(
+            'type' => $this->type,
+            'errorMsgcounts' => $this->errMsgcounter,
+            'errorMessage' => $this->errmessage,
+            'successMsgcounts' => $this->successMsgcounter,
+            'successMessage' => $this->successmessage
+        );
+    }
+
+    public function printJsonMsg()
+    {
         echo json_encode($this->printMessage());
     }
 }
@@ -46,4 +62,3 @@ class Message{
 // $msg->pushSuccessMsg("xyz");
 // $msg->pushErrorMsg("abc");
 // $msg->printJsonMsg();
-?>
